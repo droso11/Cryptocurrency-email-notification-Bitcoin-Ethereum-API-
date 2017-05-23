4744d05744cc7410e63ee11bc69a1bf1f407587e
droso11 <metin2x2@gmail.com>
2017-05-23 15:56:38 +0200

    Currencies api

---
+++ currencies_api/btc_usd.py
@@ -0,0 +1,28 @@
+# https://bitbay.net/API/Public/ETHUSD/ticker.json
+'''
+
+@json return:
+max
+min
+last
+bid
+ask
+vwmap
+average
+volume
+'''
+
+from request_helper import get_json
+
+def get_bitcoin_price():
+    json_dictionary = get_json('https://bitbay.net/API/Public/BTCUSD/ticker.json')
+
+    return json_dictionary.get('last', None)
+
+
+def get_bitcoin_list():
+    # return [min price within 24h, last, max price within 24h]
+    json_dictionary = get_json('https://bitbay.net/API/Public/BTCUSD/ticker.json')
+
+    return [json_dictionary.get('min', None),json_dictionary.get('last', None),json_dictionary.get('max', None)]
+
