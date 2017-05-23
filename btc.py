7d1d5047172e6616ece7cd1ee7f9cf4d64293595
droso11 <metin2x2@gmail.com>
2017-05-23 13:45:48 +0200

    PLN TO USD

--- btc.py
+++ btc.py
@@ -15,14 +15,14 @@ volume
 from request_helper import get_json
 
 def get_bitcoin_price():
-    json_dictionary = get_json('https://bitbay.net/API/Public/BTCPLN/ticker.json')
+    json_dictionary = get_json('https://bitbay.net/API/Public/BTCUSD/ticker.json')
 
     return json_dictionary.get('last', None)
 
 
 def get_bitcoin_list():
     # return [min price within 24h, last, max price within 24h]
-    json_dictionary = get_json('https://bitbay.net/API/Public/BTCPLN/ticker.json')
+    json_dictionary = get_json('https://bitbay.net/API/Public/BTCUSD/ticker.json')
 
     return [json_dictionary.get('min', None),json_dictionary.get('last', None),json_dictionary.get('max', None)]
 
