af790fd14c0b15e51c0d21e0c730c002ed1d0542
droso11 <metin2x2@gmail.com>
2017-05-24 11:10:03 +0200

    USD TO PLN

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 from currency import Currency
 from mail import send_email
 
-from currencies_api import btc_usd, eth_usd         # IMPORT API !
+from currencies_api import btc_pln, eth_pln         # IMPORT API !
 
 # CONFIGURE THIS
 
@@ -12,32 +12,32 @@ SMTP_SERVER = 'smtp.gmail.com'
 PORT = 465
 
 # MAIL LOGIN AND PASSWORD (SENDER)
-LOGIN = 'MAIL_FROM@gmail.com'
-PASSWORD = 'PASSWORD'
+LOGIN = 'drozduuuu@gmail.com'
+PASSWORD = 'oC0\'R++5iZDSAmw"^"aP'
 
 RECIPENTS = [
-    'MAIL_TO@gmail.com'
+    'metin2x2@gmail.com'
 ]
 
 
 currencies = [
-                #Currency('Bitcoin', 'BTC', 'PLN', 8, 180, btc_pln.get_bitcoin_list),         # PLN
+                Currency('Bitcoin', 'BTC', 'PLN', 8, 180, btc_pln.get_bitcoin_list),         # PLN
                         # long_name, short_name, currency, min_percent, minutes to update, function to get list
-                #Currency('Ethereum', 'ETH', 'PLN', 8, 180, eth_pln.get_ethereum_list),       # PLN
+                Currency('Ethereum', 'ETH', 'PLN', 8, 180, eth_pln.get_ethereum_list),       # PLN
 
 
-                Currency('Bitcoin', 'BTC', 'USD', 8, 180, btc_usd.get_bitcoin_list),         # USD
+                #Currency('Bitcoin', 'BTC', 'USD', 8, 0, btc_usd.get_bitcoin_list),         # USD
                 # How to:
                 # Create Bitcoin cryptocurrency (BTC) from USD (currency), send message if change is more or equal 8%
                 # force sending message after 180minutes if nothing was sent in that time
 
 
-                Currency('Ethereum', 'ETH', 'USD', 8, 180, eth_usd.get_ethereum_list),       # USD
+                #Currency('Ethereum', 'ETH', 'USD', 8, 0, eth_usd.get_ethereum_list),       # USD
              ]
 
 # END
 
-#time.sleep(300)
+time.sleep(300)
 
 while True:
     title = []
@@ -49,7 +49,6 @@ while True:
         body.extend(temp_body)
         body.append('<br/>\n')           # make line between currencies
 
-    print(title,body)
 
     if len(title) > 0:
         send_email(SMTP_SERVER, PORT, LOGIN, PASSWORD, RECIPENTS,
